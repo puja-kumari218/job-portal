f1da09d866fa50fddb61069dbb87565d7280290c
Puja Kumari <pujakumarijb@gmail.com>
2025-07-24 01:04:20 +0530

    complete job listing

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,7 +1,22 @@
-import React from "react";
+import React, { useContext, useRef } from "react";
 import { assets } from "../assets/assets";
+import { AppContext } from "../context/AppContext";
 
 const Hero = () => {
+  const { setSearchFilter, setIsSearched } = useContext(AppContext);
+
+  const titleRef = useRef(null)
+
+  const locationRef = useRef(null)
+
+  const onSearch = () => {
+setSearchFilter({
+  title:titleRef.current.value,
+  location: locationRef.current.value
+})
+setIsSearched(true)
+  }
+
   return (
     <div className="container 2xl:px-20 mx-auto my-10">
       <div className="bg-gradient-to-r from-purple-800 to-purple-950 text-white py-16 text-center mx-2 rounded-xl">
@@ -19,6 +34,7 @@ const Hero = () => {
               type="text"
               placeholder="Search for jobs"
               className="max-sm:text-xs p-2 rounded outline-none w-full"
+              ref={titleRef}
             />
           </div>
           <div className="flex items-center">
@@ -27,16 +43,17 @@ const Hero = () => {
               type="text"
               placeholder="Location"
               className="max-sm:text-xs p-2 rounded outline-none w-full"
+              ref={locationRef}
             />
           </div>
-          <button className="bg-blue-600 px-6 py-2 rounded text-white m-1">
+          <button onClick={onSearch} className="bg-blue-600 px-6 py-2 rounded text-white m-1">
             Search
           </button>
         </div>
       </div>
       {/* hero section ends */}
       <div className="border border-gray-300 shadow-md mx-2 mt-5 p-6 rounded-md flex">
-        <div className=" flex justify-center gap-10 lg:gap-16 flex-wrap" >
+        <div className=" flex justify-center gap-10 lg:gap-16 flex-wrap">
           <p className="font-medium">Trusted By</p>
           <img className="h-6" src={assets.microsoft_logo} alt="" />
           <img className="h-6" src={assets.walmart_logo} alt="" />
@@ -44,6 +61,7 @@ const Hero = () => {
           <img className="h-6" src={assets.samsung_logo} alt="" />
           <img className="h-6" src={assets.amazon_logo} alt="" />
           <img className="h-6" src={assets.adobe_logo} alt="" />
+          <img className="h-6" src={assets.microsoft_logo} alt="" />
         </div>
       </div>
     </div>
